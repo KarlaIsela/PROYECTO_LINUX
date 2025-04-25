941ba793189ab812475520c63e66cf85ed08cc1b
Jose Vazquez <112033000+JAVF11@users.noreply.github.com>
2025-04-25 17:02:21 -0600

    Create Creditos.sh

---
+++ Creditos.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+mostrar_creditos() {
+cat << "EOF"
+
+                       __
+                      / _)
+             _.----._/ /
+           /          /
+       __/ (  |  (   |
+      /__.-'|_|--|_ |
+
+═══════════════════════════════════════
+         PROYECTO FINAL - LINUX
+═══════════════════════════════════════
+
+Autor: Karla Isela & Jose Angel 
+Curso: Linux - Gen 1
+Fecha: Abril 2025
+Gracias por ver :)
+
+EOF
+}
+
+mostrar_creditos
